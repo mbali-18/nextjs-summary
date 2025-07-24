9159bb093155a60597c7df191623d7e8088092b6
mbali-18 <faithfulservent18@gmail.com>
2025-07-24 13:22:47 +0200

    Extracting Dynamic Route Data

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,5 +1,14 @@
+import { useRouter } from "next/router";
+
 //our-domain.com/news/something-important
+
 function DetailPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  //send a request to the backend to fetch the news item with this ID
+
   return <h1>The Detail Page</h1>;
 }
 export default DetailPage;
