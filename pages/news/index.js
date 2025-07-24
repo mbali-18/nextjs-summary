71131e76b207f4d6bc25908c87e80d9125012c29
mbali-18 <faithfulservent18@gmail.com>
2025-07-24 14:56:56 +0200

    Linking Between Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,11 +1,16 @@
+//our-domain.com/news
 import { Fragment } from "react";
-
+import Link from "next/link";
 function NewsPage() {
   return (
     <Fragment>
-      <h1>The News Page</h1>;
+      <h1>The News Page</h1>
       <ul>
-        <li>NextJs Is A Great FrameWork</li>
+        <li>
+          <Link href="/news/newsjs-is-a-great-framework">
+            NextJs Is A Great FrameWork
+          </Link>
+        </li>
         <li>Something Else</li>
       </ul>
     </Fragment>
