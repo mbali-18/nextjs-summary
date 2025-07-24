7fdc5233b4cdf6db65fc83db2bd8b27bb9244dfb
mbali-18 <faithfulservent18@gmail.com>
2025-07-24 13:50:27 +0200

    Creating Dynamic Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,14 @@
-//our-domain.com/news
+import { Fragment } from "react";
 
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>;
+      <ul>
+        <li>NextJs Is A Great FrameWork</li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 }
 export default NewsPage;
